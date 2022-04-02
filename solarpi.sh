00315e8c7a1b4dec51034cd228509c982d87f72c
Jefta Dirksen <jeftadirksen@gmail.com>
2022-04-02 16:59:47 +0200

    Added date to output for logging

--- solarpi.sh
+++ solarpi.sh
@@ -8,6 +8,9 @@ cd "$(dirname "$0")"
 now=$(date "+%H%M")  # Get time formatted as number
 now=$((10#$now))     # Convert string to base 10 number
 
+# Current date (for logging)
+date=$(date "+%y%m%d")
+
 # Get GPIO pin state
 pin_get=($(raspi-gpio get $gpio_pin))  # Get command output as array
 pin_state=${pin_get[4]}                # Get 5th array element
@@ -15,7 +18,7 @@ pin_state=${pin_state//func=/}         # Remove func=
 
 # Setup GPIO pin
 [[ "$pin_state" != "OUTPUT" ]] && {
-  echo "$now Setting up GPIO pin $gpio_pin with state off"
+  echo "$date $now Setting up GPIO pin $gpio_pin with state off"
   raspi-gpio set $gpio_pin op  # Setup GPIO pin as Output
   raspi-gpio set $gpio_pin dh  # Set GPIO pin High (switch = off)
   exit
@@ -23,7 +26,7 @@ pin_state=${pin_state//func=/}         # Remove func=
 
 # Get weather data (on: time, parameter, missing file)
 [[ $now -eq $time_update_weather || "$1" = "-u" || ! -f weather_data.json ]] && {
-  echo "$now Getting weather data for $weather_api_location"
+  echo "$date $now Getting weather data for $weather_api_location"
   curl -s -o weather_data.json $weather_api_url
 }
 
@@ -34,35 +37,35 @@ sd=$(cat weather_data.json | jq -r ".liveweer[0].sunder")
 sd=$((10#${sd//:/}))  # Remove semicolon and leading zeros
 
 # Parameter switch on/off/state
-[[ "$1" = "-on"  ]] && raspi-gpio set $gpio_pin dl && echo "$now Switch on (manual)" && exit
-[[ "$1" = "-off" ]] && raspi-gpio set $gpio_pin dh && echo "$now Switch off (manual)" && exit
+[[ "$1" = "-on"  ]] && raspi-gpio set $gpio_pin dl && echo "$date $now Switch on (manual)" && exit
+[[ "$1" = "-off" ]] && raspi-gpio set $gpio_pin dh && echo "$date $now Switch off (manual)" && exit
 [[ "$1" = "-state" ]] && {
   switch_state=${pin_get[2]}             # Get 3rd array element
   switch_state=${switch_state//level=/}  # Remove level=
   [[ switch_state -eq 1 ]] && switch_state=Off || switch_state=On
-  echo "$now Switch: $switch_state  Sun up/down: $su/$sd ($weather_api_location)"
+  echo "$date $now Switch: $switch_state  Sun up/down: $su/$sd ($weather_api_location)"
   exit
 }
 
 # Switch on/off
 [[ $su -gt $time_on ]] && {            # Sun up after time on
   [[ $now -eq $time_on ]] && {         # @ time on
-    echo "$now Switch on (time on)"
+    echo "$date $now Switch on (time on)"
     raspi-gpio set $gpio_pin dl        # Switch on
   }
   [[ $now -eq $su ]] && {              # @ sun up
-    echo "$now Switch off (sun up)"
+    echo "$date $now Switch off (sun up)"
     raspi-gpio set $gpio_pin dh        # Switch off
   }
 }
 
 [[ $sd -lt $time_off ]] && {           # Sun down before time off
   [[ $now -eq $sd ]] && {              # @ sun down
-    echo "$now Switch on (sun down)"
+    echo "$date $now Switch on (sun down)"
     raspi-gpio set $gpio_pin dl        # Switch on
   }
   [[ $now -eq $time_off ]] && {        # @ time off
-    echo "$now Switch off (time off)"
+    echo "$date $now Switch off (time off)"
     raspi-gpio set $gpio_pin dh        # Switch off
   }
 }
