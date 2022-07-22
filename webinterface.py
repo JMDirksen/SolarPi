7af2d708fc291385777fb476f65e9e3c96373236
Jefta Dirksen <jeftadirksen@gmail.com>
2022-07-22 10:22:20 +0200

    Added state to webinterface

--- webinterface.py
+++ webinterface.py
@@ -1,13 +1,15 @@
 from bottle import route, run, redirect
 from os import system
+from os import popen
 
 @route('/')
 def index():
-  header = '<html><head><meta name="viewport" content="width=device-width, initial-scale=3.0"><title>SolarPi</title></head><body bgcolor="black">'
+  header = '<html><head><meta name="viewport" content="width=device-width, initial-scale=3.0"><title>SolarPi</title></head><body style="color:white; background-color:black; font-family: courier, serif;">'
+  state = 'State: ' + popen('./solarpi.sh -state').read().split()[3] + '<br/><br/>'
   buttonOn = '<button onClick="location.href=\'/on\'" style="color:white; background-color:green">On</button>'
   buttonOff = '<button onClick="location.href=\'/off\'" style="color:white; background-color:red">Off</button>'
   footer = '</body></html>'
-  buttons = header + buttonOn + ' ' + buttonOff + footer
+  buttons = header + state + buttonOn + ' ' + buttonOff + footer
   return buttons
 
 @route('/on')
